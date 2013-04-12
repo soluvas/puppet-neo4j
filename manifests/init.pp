7dc4f1f73ccbd054d785ea01286e6b7696ceb68d
Hendy Irawan <ceefour666@gmail.com>
2013-04-12 11:34:27 +0700

    upgrade neo4j to 1.8.2

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,7 +3,7 @@
 # For installing neo4j server.
 #
 class neo4j (
-  $release             = 'neo4j-community-1.8.1',
+  $release             = 'neo4j-community-1.8.2',
   $mirror_url          = 'http://dist.neo4j.org/',
   $bind_address        = '127.0.0.1',
   $allow_store_upgrade = false,
