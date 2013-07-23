449c970214a7242616a664a92406c2ddde92e04e
Hendy Irawan <ceefour666@gmail.com>
2013-07-24 05:22:49 +0700

    deb repo

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,14 +3,23 @@
 # For installing neo4j server.
 #
 class neo4j (
-  $release             = 'neo4j-community-1.8.2',
-  $mirror_url          = 'http://dist.neo4j.org/',
+  $release             = 'neo4j-community-1.8.2', # obsolete
+  $mirror_url          = 'http://dist.neo4j.org/', # obsolete
   $bind_address        = '127.0.0.1',
   $allow_store_upgrade = false,
   $enable_service      = true)
 {
+  apt::source { neo4j:
+    location    => 'http://debian.neo4j.org/repo',
+    release     => 'stable/',
+    repos       => '',
+    key         => '2DC499C3',
+    key_server  => 'keyserver.ubuntu.com',
+    include_src => false,
+  }
   package { neo4j:
-    ensure => present,
+    ensure  => present,
+    require => Apt::Source['neo4j'],
   }
   service { neo4j-service:
     enable     => $enable_service,
